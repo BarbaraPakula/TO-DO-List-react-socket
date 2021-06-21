561e3ec9bf79e3ecc66fc9a31f686e123acae4fc
Barbara Pakula <barbara.pakula@hotmail.com>
2021-06-21 22:27:15 +0200

    Add functionality to app

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 const express = require('express');
 const socket = require('socket.io');
+const cors = require('cors');
 
 const app = express();
+app.use(cors(
+
+));
+
+
+const tasks = [];
+console.log('tasks', tasks);
+
 
 app.use((req, res) => {
   res.status(404).json({ message: 'Not found...' });
@@ -11,4 +20,26 @@ const server = app.listen(process.env.PORT || 8000, () => {
   console.log('Server is running on port: 8000');
 });
 
-const io = socket(server); 
+const io = socket(server, {
+    cors: {
+      origin: '*',
+      methods: ['GET', 'POST']
+    }
+})
+io.on('connection', (socket) => {
+  console.log('New client - its id: ', socket.id);
+  io.to(socket.id).emit('updateData', tasks);
+
+  socket.on('addTask', (newTask) => {
+    console.log('New task in array: ', newTask, '(from user:' + socket.id + ')');
+    tasks.push(newTask);
+    socket.broadcast.emit('addTask', newTask);
+  });
+
+  socket.on('removeTask', (id) => {
+    const index = tasks.indexOf(tasks.find(task => task.id === id));
+    console.log('Task removed - its id:', id);
+    tasks.splice(index, 1);
+    socket.broadcast.emit('removeTask', id);
+  });
+});
